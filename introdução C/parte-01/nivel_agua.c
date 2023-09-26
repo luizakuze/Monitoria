421236ea0d01be0571a7166da45341b3d3411e86
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-09-26 10:53:07 -0300

    Update nivel_agua.c

--- introdução C/parte-01/nivel_agua.c
+++ introdução C/parte-01/nivel_agua.c
@@ -26,3 +26,39 @@ int main(void)
 
     return 0;
 }
+
+// A variável "transbordou" como booleana
+/* funcina tbm!!*/
+
+// Outra ideia...
+
+/* #include <stdio.h>
+#define CAPACIDADE 400.0  // litros
+
+int main(void)
+{
+    float nivel = 0.0, chuva;
+    bool transbordou = false;
+    int dia = 1;
+
+    while (!transbordou && dia <= 7) {
+        printf("Entre com a quantidade de chuva no dia %d em litros: ", dia);
+        scanf("%f", &chuva);
+        nivel += chuva;
+        if (nivel >= CAPACIDADE) {
+            transbordou = true;
+            continue;
+        }
+        dia++;
+        printf("Nível do reservatório: %.1f litros\n\n", nivel);
+    }
+
+    if (transbordou) {
+        printf("O reservatório transbordou no dia %d\n", dia);
+    } else {
+        printf("O reservatório não transbordou após uma semana, com %.1f litros livres.\n", CAPACIDADE - nivel);
+    }
+
+    return 0;
+}
+*/
