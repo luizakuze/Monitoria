b39c93b917f23e01289ab9419154f385067fbf9a
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-09-28 16:45:24 -0300

    Update acima_media.c

--- introdução C/parte-01/acima_media.c
+++ introdução C/parte-01/acima_media.c
@@ -1,3 +1,6 @@
+// implementar um programa C para ler 10 números inteiros para um vetor
+// e imprimir a quantidade (o número) de números acima da média.
+
 #include <stdio.h>
 
 #define NUM_ELEMENTOS 10
@@ -6,25 +9,25 @@ int main() {
     int vet[NUM_ELEMENTOS];
     int soma_num = 0;
 
-    // Leitura dos números inteiros para o vetor
+    // leitura dos números inteiros para o vetor
     for (int i = 0; i < NUM_ELEMENTOS; i++) {
         printf("vet[%d] = ", i);
         scanf("%d", &vet[i]);
         soma_num += vet[i];
     }
 
-    // Cálculo da média
+    // cálculo da média
     float media = (float)soma_num / NUM_ELEMENTOS;
     int acima_media = 0;
 
-    // Contagem de números acima da média
+    // contagem de números acima da média
     for (int i = 0; i < NUM_ELEMENTOS; i++) {
         if (vet[i] > media) {
             acima_media++;
         }
     }
 
-    // Impressão do resultado
+    // impressão do resultado
     printf("Existem %d números acima da média.\n", acima_media);
 
     return 0;
